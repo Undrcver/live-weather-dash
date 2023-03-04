17ed28896f938b0d264be745dc7ef9738f404ae4
Louie <Loualicea@hotmail.com>
2023-03-04 14:25:53 -0500

    updated some script added image

--- assets/script.js
+++ assets/script.js
@@ -50,92 +50,92 @@ var getApis = function () {
                 })
                 .then(function (data) {
                     console.log(data);
-                    
+
                 })
-                weekHeader.textContent = 'weekly Forecast:';
-                
-                first.classList.add('day-container');
-                second.classList.add('day-container');
-                third.classList.add('day-container');
-                fourth.classList.add('day-container');
-                fifth.classList.add('day-container');
+            weekHeader.textContent = 'Weekly Forecast:';
 
-                var firstDate = document.createElement('h4');
-                var secondDate = document.createElement('h4');
-                var thirdDate = document.createElement('h4');
-                var fourthDate = document.createElement('h4');
-                var fifthDate = document.createElement('h4');
+            first.classList.add('day-container');
+            second.classList.add('day-container');
+            third.classList.add('day-container');
+            fourth.classList.add('day-container');
+            fifth.classList.add('day-container');
 
-                var firstTemp = document.createElement('p');
-                var secondTemp = document.createElement('p');
-                var thirdTemp = document.createElement('p');
-                var fourthTemp = document.createElement('p');
-                var fifthTemp = document.createElement('p');
+            var firstDate = document.createElement('h4');
+            var secondDate = document.createElement('h4');
+            var thirdDate = document.createElement('h4');
+            var fourthDate = document.createElement('h4');
+            var fifthDate = document.createElement('h4');
 
-                var firstWind = document.createElement('p');
-                var secondWind = document.createElement('p');
-                var thirdWind = document.createElement('p');
-                var fourthWind = document.createElement('p');
-                var fifthWind = document.createElement('p');
+            var firstTemp = document.createElement('p');
+            var secondTemp = document.createElement('p');
+            var thirdTemp = document.createElement('p');
+            var fourthTemp = document.createElement('p');
+            var fifthTemp = document.createElement('p');
 
-                var firstHumidity = document.createElement('p');
-                var secondHumidity = document.createElement('p');
-                var thirdHumidity = document.createElement('p');
-                var fourthHumidity = document.createElement('p');
-                var fifthHumidity = document.createElement('p');
+            var firstWind = document.createElement('p');
+            var secondWind = document.createElement('p');
+            var thirdWind = document.createElement('p');
+            var fourthWind = document.createElement('p');
+            var fifthWind = document.createElement('p');
+
+            var firstHumidity = document.createElement('p');
+            var secondHumidity = document.createElement('p');
+            var thirdHumidity = document.createElement('p');
+            var fourthHumidity = document.createElement('p');
+            var fifthHumidity = document.createElement('p');
 
 
-                // day forcast //
+            // day forcast //
 
-                firstDate.textContent = data.daily[0].dt;
-                firstTemp.textContent = 'Temp: ' + data.daily[0].temp.day + ' °F';
-                firstWind.textContent = 'Wind: ' + data.daily[0].wind_speed + ' MPH';
-                firstHumidity.textContent = 'Humidity: ' + data.daily[0].humidity + '%';
+            firstDate.textContent = data.daily[0].dt;
+            firstTemp.textContent = 'Temp: ' + data.daily[0].temp.day + ' °F';
+            firstWind.textContent = 'Wind: ' + data.daily[0].wind_speed + ' MPH';
+            firstHumidity.textContent = 'Humidity: ' + data.daily[0].humidity + '%';
 
-                first.appendChild(firstDate);
-                first.appendChild(firstTemp);
-                first.appendChild(firstWind);
-                first.appendChild(firstHumidity);
+            first.appendChild(firstDate);
+            first.appendChild(firstTemp);
+            first.appendChild(firstWind);
+            first.appendChild(firstHumidity);
 
-                secondDate.textContent = data.daily[1].dt;
-                secondTemp.textContent = 'Temp: ' + data.daily[1].temp.day + ' °F';
-                secondWind.textContent = 'Wind: ' + data.daily[1].wind_speed + ' MPH';
-                secondHumidity.textContent = 'Humidity: ' + data.daily[1].humidity + '%';
+            secondDate.textContent = data.daily[1].dt;
+            secondTemp.textContent = 'Temp: ' + data.daily[1].temp.day + ' °F';
+            secondWind.textContent = 'Wind: ' + data.daily[1].wind_speed + ' MPH';
+            secondHumidity.textContent = 'Humidity: ' + data.daily[1].humidity + '%';
 
-                second.appendChild(secondDate);
-                second.appendChild(secondTemp);
-                second.appendChild(secondWind);
-                second.appendChild(secondHumidity);
+            second.appendChild(secondDate);
+            second.appendChild(secondTemp);
+            second.appendChild(secondWind);
+            second.appendChild(secondHumidity);
 
-                thirdDate.textContent = data.daily[2].dt;
-                thirdTemp.textContent = 'Temp: ' + data.daily[2].temp.day + ' °F';
-                thirdWind.textContent = 'Wind: ' + data.daily[2].wind_speed + ' MPH';
-                thirdHumidity.textContent = 'Humidity: ' + data.daily[2].humidity + '%';
+            thirdDate.textContent = data.daily[2].dt;
+            thirdTemp.textContent = 'Temp: ' + data.daily[2].temp.day + ' °F';
+            thirdWind.textContent = 'Wind: ' + data.daily[2].wind_speed + ' MPH';
+            thirdHumidity.textContent = 'Humidity: ' + data.daily[2].humidity + '%';
 
-                third.appendChild(thirdDate);
-                third.appendChild(thirdTemp);
-                third.appendChild(thirdWind);
-                third.appendChild(thirdHumidity);
+            third.appendChild(thirdDate);
+            third.appendChild(thirdTemp);
+            third.appendChild(thirdWind);
+            third.appendChild(thirdHumidity);
 
-                fourthDate.textContent = data.daily[3].dt;
-                fourthTemp.textContent = 'Temp: ' + data.daily[3].temp.day + ' °F';
-                fourthWind.textContent = 'Wind: ' + data.daily[3].wind_speed + ' MPH';
-                fourthHumidity.textContent = 'Humidity: ' + data.daily[3].humidity + '%';
+            fourthDate.textContent = data.daily[3].dt;
+            fourthTemp.textContent = 'Temp: ' + data.daily[3].temp.day + ' °F';
+            fourthWind.textContent = 'Wind: ' + data.daily[3].wind_speed + ' MPH';
+            fourthHumidity.textContent = 'Humidity: ' + data.daily[3].humidity + '%';
 
-                fourth.appendChild(fourthDate);
-                fourth.appendChild(fourthTemp);
-                fourth.appendChild(fourthWind);
-                fourth.appendChild(fourthHumidity);
+            fourth.appendChild(fourthDate);
+            fourth.appendChild(fourthTemp);
+            fourth.appendChild(fourthWind);
+            fourth.appendChild(fourthHumidity);
 
-                fifthDate.textContent = data.daily[4].dt;
-                fifthTemp.textContent = 'Temp: ' + data.daily[4].temp.day + ' °F';
-                fifthWind.textContent = 'Wind: ' + data.daily[4].wind_speed + ' MPH';
-                fifthHumidity.textContent = 'Humidity: ' + data.daily[4].humidity + '%';
+            fifthDate.textContent = data.daily[4].dt;
+            fifthTemp.textContent = 'Temp: ' + data.daily[4].temp.day + ' °F';
+            fifthWind.textContent = 'Wind: ' + data.daily[4].wind_speed + ' MPH';
+            fifthHumidity.textContent = 'Humidity: ' + data.daily[4].humidity + '%';
 
-                fifth.appendChild(fifthDate);
-                fifth.appendChild(fifthTemp);
-                fifth.appendChild(fifthWind);
-                fifth.appendChild(fifthHumidity);
+            fifth.appendChild(fifthDate);
+            fifth.appendChild(fifthTemp);
+            fifth.appendChild(fifthWind);
+            fifth.appendChild(fifthHumidity);
         })
 };
 
